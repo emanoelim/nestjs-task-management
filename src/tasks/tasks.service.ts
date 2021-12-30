6c7da291a68420a45247acc3bc460b3ae9542536
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2021-12-30 16:52:38 -0300

    testes

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,9 +5,10 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TasksRespository } from './tasks.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 @Injectable()
 export class TasksService {
+
     constructor(
         @InjectRepository(TasksRespository)
         private tasksRepository: TasksRespository,
